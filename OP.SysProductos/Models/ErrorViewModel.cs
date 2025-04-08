9ffc0aa2c16245c4c21e4462e04c6c8c650d2798
MINED <MINED@DESKTOP-4TMNANE>
2025-04-07 19:11:33 -0600

    Agregar archivos de proyecto.

---
+++ OP.SysProductos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PR.SysProductos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
